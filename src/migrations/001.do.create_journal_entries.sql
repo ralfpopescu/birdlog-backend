1e407bef9b3d98a48bbc53a57e725e4e16276478
Ralf Popescu <ralfpopescu@gmail.com>
2018-06-20 12:33:43 -0400

    initial commit

---
+++ src/migrations/001.do.create_journal_entries.sql
@@ -0,0 +1,6 @@
+CREATE TABLE journal_entries (
+  month number NOT NULL,
+  day number NOT NULL,
+  year number NOT NULL,
+  entry text
+);
---
+++ src/schema/schema.js
@@ -0,0 +1,6 @@
+type entry {
+  month: Int
+  day: Int
+  year: Int
+  entry: String
+}
